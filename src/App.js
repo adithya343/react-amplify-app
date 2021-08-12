9db164ab65fd9b1217b698b77b386da7bbc1c1ce
Adi343 <53225414+Adi343@users.noreply.github.com>
2021-08-12 20:16:35 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello amplify.
+          Hello amplify updated.
         </p>
         
       </header>
